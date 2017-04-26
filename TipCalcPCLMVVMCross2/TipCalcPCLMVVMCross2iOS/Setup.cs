7b23cb26d5a1864ad6180efefc7d5cc72b531d5a
Uncle88 <andreyproskura88@gmail.com>
2017-04-26 15:47:48 +0300

    created class Setup

---
+++ TipCalcPCLMVVMCross2/TipCalcPCLMVVMCross2iOS/Setup.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using TipCalcPCLMVVMCross2;
+
+namespace TipCalcPCLMVVMCross2iOS
+{
+    public class Setup : MvxIosSetup
+    {
+        public Setup(MvxApplicationDelegate appDelegate, IMvxIosViewPresenter presenter)
+            : base(appDelegate, presenter)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+    }
+}
