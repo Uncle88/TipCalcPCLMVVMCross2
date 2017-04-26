a399bcf58cee0a1d67e99e4216e0aad6ea15bf1f
Uncle88 <andreyproskura88@gmail.com>
2017-04-26 14:01:45 +0300

    created view for droid

---
+++ TipCalcPCLMVVMCross2/TipCalcPCLMVVMCross2Droid/Views/TipView.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+using TipCalcPCLMVVMCross2.ViewModels;
+
+namespace TipCalcPCLMVVMCross2Droid.Views
+{
+    [Activity(Label = "Tip", MainLauncher = true)]
+    public class TipView : MvxActivity
+    {
+        public new TipViewModel ViewModel
+        {
+            get { return (TipViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+            SetContentView(Resource.Layout.ViewTip);
+        }
+    }
+}
